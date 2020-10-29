a442201f739eaaad2e6f35cb1364b65bb8d1e5e2
Brendan <brendanewen@gmail.com>
2020-10-29 19:02:41 +0000

    added some xtra gulp atoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var rename = require( 'gulp-rename' );
 var sass = require( 'gulp-sass' );
 var minifyCSS = require('gulp-clean-css');
 var changed = require('gulp-changed');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 var styleSRC = './src/scss/style.scss';
 var styleDIST = './dist/css/';
@@ -10,12 +12,15 @@ var styleDIST = './dist/css/';
 
 gulp.task('style', function(){
   return gulp.src( styleSRC )
+      .pipe( sourcemaps.init() )
       .pipe( sass({
         errorLogToConsole: true,
       }) )
       .on( 'error', console.error.bind( console ) )
+      .pipe( autoprefixer()  )
       .pipe(minifyCSS())
       .pipe( rename( { suffix: '.min' } ) )
+      .pipe( sourcemaps.write('./') )
       .pipe(changed(styleDIST))
       .pipe( gulp.dest( styleDIST ) );
 });
